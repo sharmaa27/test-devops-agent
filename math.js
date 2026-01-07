3070920d5e7d795065b3fbdf2bcdf503d9ae108f
Vipul Sharma <123194067+sharmaa27@users.noreply.github.com>
2026-01-08 00:37:28 +0530

    Delete math.js

--- math.js
+++
@@ -1,6 +0,0 @@
-function add(a, b) {
-    return a + b
-    // Missing semicolon (not a huge bug, but let's see)
-    // Or better:
-    retrun a + b; // SPELLING ERROR! "retrun" instead of "return"
-}
