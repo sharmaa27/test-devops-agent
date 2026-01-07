1a26b7dc3a9bc85fdf3075da3dddb351c6fb9a18
Vipul Sharma <123194067+sharmaa27@users.noreply.github.com>
2026-01-08 00:38:14 +0530

    math.js

---
+++ math.js
@@ -0,0 +1,6 @@
+function add(a, b) {
+    return a + b
+    // Missing semicolon (not a huge bug, but let's see)
+    // Or better:
+    retrun a + b; // SPELLING ERROR! "retrun" instead of "return"
+}
